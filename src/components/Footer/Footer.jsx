7b9818479e4b5cd9a218584af2688f6d04df77ca
LorsenLamour <sirvalentino0@gmail.com>
2025-12-05 14:18:07 -0500

    Added a footer picture that have a few skills

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,15 @@
 import "../Footer/Footer.css"
-import footerpng from "../../assets/Footer_bg.jpg"
+import footerpng from "../../assets/Footer_picture.jpg"
 function Footer() {
     return (
         <div className="footer-style">
-            <div className="image-footer-style">
+         
                 <img className="image-footer-style" src={footerpng} alt="Description de l'image"></img>
+         
                 <p className="footer-text-style">&copy; 2024 Mon Site Web</p>
+    
             </div>
-        </div>
+ 
     )
 }
 export default Footer;
